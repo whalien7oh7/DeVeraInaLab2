9091cf213833113437ba7e8b9dc47d32b5209eff
whalien7oh7 <2015081484@ust-ics.mygbiz.com>
2018-09-04 00:03:51 +0800

    redo

---
+++ app/src/main/java/com/devera/ina/DeVeraInaLab1.java
@@ -0,0 +1,15 @@
+package com.devera.ina;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DeVeraInaLab1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.linear_layout);
+//        setContentView(R.layout.relative_layout);
+//        setContentView(R.layout.constraint_layout);
+    }
+}
